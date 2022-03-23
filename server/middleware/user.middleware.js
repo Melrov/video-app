953b402514ea7339a32a634a351bd5695d0b0b27
Melrov <rdunham918@gmail.com>
2022-03-23 12:16:43 -0500

    fixed valid lengths not working bug

--- server/middleware/user.middleware.js
+++ server/middleware/user.middleware.js
@@ -3,7 +3,7 @@ function validateInput(req, res, next) {
   if (!b.username || !b.password) {
     return res.send({ success: false, data: null, error: "Invalid data provided" });
   }
-  if (!b.username.length >= 2 || !b.username <= 25 || !b.password >= 4 || !b.password <= 512) {
+  if (!(b.username.length >= 2) || !(b.username.length <= 25) || !(b.password.length >= 4) || !(b.password.length <= 512)) {
     return res.send({ success: false, data: null, error: "Invalid data provided" });
   }
   return next()
