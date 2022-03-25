f6bb49174c152eead9d21fa448cc35080aefa5a8
Melrov <rdunham918@gmail.com>
2022-03-25 16:56:20 -0500

    started on video preview

--- src/components/Reusable/VideoPreview.jsx
+++ src/components/Reusable/VideoPreview.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { ViewSidebar } from "@mui/icons-material";
+import React from "react";
+import "./VideoPreview.css";
 
-function VideoPreview({ thumbnail, tooltip, Title, username, views, uploadDate }) {
+function VideoPreview({ thumbnail, tooltip, title, username, views, uploadDate, version = "row" }) {
   return (
-    <div>VideoPreview</div>
-  )
+    <div className={`container-${version === "column" ? "col" : "row"}`}>
+      <div className="thumbnail-con">
+        <img className="img" src={thumbnail} />
+        <div className="tooltip">{tooltip}</div>
+      </div>
+      <div className="info-con">
+        <span>{title}</span> <br/>
+        <span>{username}</span> <br/>
+        <span>{views + " views " + uploadDate}</span>
+      </div>
+    </div>
+  );
 }
 
-export default VideoPreview
+export default VideoPreview;
