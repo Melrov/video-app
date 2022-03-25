6a0d6a81238ca5da7ae2d8b9a88592b8db7b30cd
Melrov <rdunham918@gmail.com>
2022-03-25 12:24:01 -0500

    creates routes for componets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import VideoPage from "./components/VideoPage";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path='' element={<></>}/>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/video/:contentId" element={<VideoPage />} />
       </Routes>
     </Router>
   );
