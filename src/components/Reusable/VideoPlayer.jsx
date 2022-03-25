a81aa489a1a04cbb013589cd01bc86f2cf3135cb
Melrov <rdunham918@gmail.com>
2022-03-25 16:56:04 -0500

    started on video player

--- src/components/Reusable/VideoPlayer.jsx
+++ src/components/Reusable/VideoPlayer.jsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import React, { useCallback, useRef } from "react";
+import "./VideoPlayer.css";
+import useVideoPlayer from "../../hooks/useVideoPlayer";
+import styled from "styled-components";
 
-function VideoPlayer() {
+const Input = styled.input`
+  -webkit-appearance: none;
+  width: calc(100% - 5px);
+  height: 7px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 4px;
+  background-image: linear-gradient(rgb(255 69 0 / 100%), rgb(255 69 0 / 20%));
+  background-size: 70% 100%;
+  background-repeat: no-repeat;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 0px;
+    width: 0px;
+    border-radius: 50%;
+    background: #ff4500;
+    cursor: pointer;
+    box-shadow: 0 0 2px 0 #555;
+    transition: all 0.125s ease-in-out;
+  }
+
+  &::-webkit-slider-runnable-track {
+    -webkit-appearance: none;
+    box-shadow: none;
+    border: none;
+    background: transparent;
+    cursor: pointer;
+  }
+`;
+
+const InputContainer = styled.div`
+  &:hover {
+    ${Input} {
+      &::-webkit-slider-thumb {
+        height: 15px;
+        width: 15px;
+      }
+    }
+  }
+`;
+
+const VideoPlayer = ({ url }) => {
+  const videoElement = useRef(null);
+  const inputRef = useRef();
+  const playBtn = useRef();
+  const timeRef = useRef();
+  const controlsRef = useRef();
+  const volumeBtnRef = useRef();
+  const {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoSpeed,
+    toggleMute,
+    loadedData,
+    input,
+    rewind,
+    forward,
+    ended,
+    volumeChange,
+  } = useVideoPlayer(videoElement, inputRef, playBtn, timeRef, controlsRef, volumeBtnRef);
+
   return (
-    <div>VideoPlayer</div>
-  )
-}
+    <div className="video-wrapper">
+      <video
+        src={url}
+        ref={videoElement}
+        onTimeUpdate={handleOnTimeUpdate}
+        onLoadedData={loadedData}
+        onEnded={ended}
+        controls={false}
+        disablePictureInPicture={true}
+        autoPlay={false}
+      />
+      <div className="progressAreaTime" ref={timeRef}>
+        0:00
+      </div>
+      <div className="controls" ref={controlsRef}>
+        <InputContainer>
+          <Input
+            type="range"
+            min="0"
+            max="100"
+            value={playerState.progress}
+            onChange={input.change}
+            onMouseMove={input.mouseMove}
+            onMouseLeave={input.mouseLeave}
+            onMouseDown={input.mouseDown}
+            onMouseUp={input.mouseUp}
+            ref={inputRef}
+          />
+        </InputContainer>
+        <div className="actions">
+          <div className="actions-left">
+            <button class="icon">
+              <i class="material-icons fast-rewind" onClick={rewind}>
+                replay_10
+              </i>
+            </button>
+            <button onClick={togglePlay}>
+              <i className="material-icons play_pause" ref={playBtn}>
+                play_arrow
+              </i>
+            </button>
+            <button class="icon">
+              <i class="material-icons fast-forward" onClick={forward}>
+                forward_10
+              </i>
+            </button>
+            <div className="timer">
+              <span>
+                {playerState.current} / {playerState.duration}
+              </span>
+            </div>
+            <button class="icon volume_box">
+              <i class="material-icons volume" ref={volumeBtnRef} onClick={toggleMute}>
+                volume_up
+              </i>
+              <input type="range" min="0" max="100" class="volume_range" value={playerState.volume} onChange={volumeChange} />
+            </button>
+          </div>
+          <div className="actions-right">
+            <select className="velocity" value={playerState.speed} onChange={(e) => handleVideoSpeed(e)}>
+              <option value="0.50">0.50x</option>
+              <option value="1">1x</option>
+              <option value="1.25">1.25x</option>
+              <option value="2">2x</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
--- src/hooks/useVideoPlayer.jsx
+++ src/hooks/useVideoPlayer.jsx
@@ -1,7 +1,184 @@
-import React from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 
-const useVideoPlayer = () => {
-  return {};
+const useVideoPlayer = (videoElement, inputRef, playBtn, timeRef, controlsRef, volumeBtnRef) => {
+  const [playerState, setPlayerState] = useState({
+    wasPlaying: false,
+    isPlaying: false,
+    progress: 0,
+    speed: 1,
+    isMuted: false,
+    current: "0 : 00",
+    duration: "0 : 00",
+    volume: 100,
+    wasVolume: 100,
+  });
+
+  useEffect(() => {
+    if(!playerState.isPlaying){
+      controlsRef.current.classList.add("paused")
+    } else {
+      controlsRef.current.classList.remove("paused")
+    }
+  }, [playerState])
+
+  const loadedData = useCallback(
+    (e) => {
+      let videoDuration = e.target.duration;
+      let totalMin = Math.floor(videoDuration / 60);
+      let totalSec = Math.floor(videoDuration % 60);
+
+      // if seconds are less then 10 then add 0 at the begning
+      totalSec = totalSec < 10 ? "0" + totalSec : totalSec;
+      setPlayerState({ ...playerState, duration: `${totalMin} : ${totalSec}` });
+      togglePlay()
+    },
+    [playerState]
+  );
+
+  const togglePlay = () => {
+    const playing = playerState.isPlaying;
+    playBtn.current.innerText = playing ? "play_arrow" : "pause_arrow";
+    setPlayerState((curr) => {
+      return { ...curr, isPlaying: !playing };
+    });
+    setPlayerState((curr) => {
+      return { ...curr, wasPlaying: !playing };
+    });
+  };
+
+  useEffect(() => {
+    playerState.isPlaying ? videoElement.current.play() : videoElement.current.pause();
+  }, [playerState.isPlaying, videoElement]);
+
+  const handleOnTimeUpdate = (e) => {
+    let currentVideoTime = videoElement.current.currentTime;
+    let currentMin = Math.floor(currentVideoTime / 60);
+    let currentSec = Math.floor(currentVideoTime % 60);
+    // if seconds are less then 10 then add 0 at the begning
+    currentSec = currentSec < 10 ? "0" + currentSec : currentSec;
+    const progress = (videoElement.current.currentTime / videoElement.current.duration) * 100;
+    setPlayerState({
+      ...playerState,
+      progress,
+      current: `${currentMin} : ${currentSec}`,
+    });
+  };
+
+  // input element events
+  const input = useMemo(() => {
+    return {
+      change: (e) => {
+        const manualChange = Number(e.target.value);
+        videoElement.current.currentTime = (videoElement.current.duration / 100) * manualChange;
+        setPlayerState({
+          ...playerState,
+          progress: manualChange,
+        });
+      },
+      mouseMove: (e) => {
+        let progressWidthval = inputRef.current.clientWidth;
+        let x = e.nativeEvent.offsetX;
+        timeRef.current.style.setProperty("--x", `${x}px`);
+        timeRef.current.style.display = "block";
+        let videoDuration = videoElement.current.duration;
+        let progressTime = Math.floor((x / progressWidthval) * videoDuration);
+        let currentMin = Math.floor(progressTime / 60);
+        let currentSec = Math.floor(progressTime % 60);
+        // if seconds are less then 10 then add 0 at the begning
+        currentSec = currentSec < 10 ? "0" + currentSec : currentSec;
+        timeRef.current.innerHTML = `${currentMin} : ${currentSec}`;
+      },
+      mouseLeave: (e) => {
+        timeRef.current.style.display = "none";
+        console.log(videoElement.current.buffered.end(0) / videoElement.current.duration);
+      },
+      mouseDown: (e) => {
+        playBtn.current.innerText = "play_arrow";
+        setPlayerState((curr) => {
+          return { ...curr, isPlaying: false };
+        });
+      },
+      mouseUp: (e) => {
+        if(playerState.wasPlaying){
+          playBtn.current.innerText = "pause_arrow"
+        }
+        setPlayerState((curr) => {
+          return { ...curr, isPlaying: curr.wasPlaying };
+        });
+      },
+    };
+  }, [videoElement, inputRef, timeRef, playerState]);
+
+  useEffect(() => {
+    if (inputRef) {
+      inputRef.current.style.backgroundSize = `${playerState.progress}% 100%`;
+    }
+  }, [playerState.progress, inputRef]);
+
+  const handleVideoSpeed = (event) => {
+    const speed = Number(event.target.value);
+    videoElement.current.playbackRate = speed;
+    setPlayerState({
+      ...playerState,
+      speed,
+    });
+  };
+
+  const toggleMute = () => {
+    volumeBtnRef.current.innerText = !playerState.isMuted ? 'volume_off' : playerState.volume <= 40 ? 'volume_down' : 'volume_up';
+    setPlayerState({
+      ...playerState,
+      isMuted: !playerState.isMuted,
+      volume: !playerState.isMuted ? 0 : playerState.wasVolume > 0 ? playerState.wasVolume : 1
+    });
+  };
+
+  useEffect(() => {
+    playerState.isMuted ? (videoElement.current.muted = true) : (videoElement.current.muted = false);
+  }, [playerState.isMuted, videoElement]);
+
+  const rewind = useCallback(() => {
+      videoElement.current.currentTime-=10
+  }, [videoElement])
+
+  const forward = useCallback(() => {
+      videoElement.current.currentTime+=10
+  }, [videoElement])
+
+  const ended = useCallback(() => {
+    setPlayerState((curr) => {
+      return { ...curr, wasPlaying: false, isPlaying: false };
+    });
+    playBtn.current.innerText = "replay"
+  }, [])
+
+  const volumeChange = useCallback((e) => {
+    if(parseInt(e.target.value) === 0){
+      toggleMute()
+    }
+    volumeBtnRef.current.innerText = parseInt(e.target.value) === 0 ? 'volume_off' : parseInt(e.target.value) <= 40 ? 'volume_down' : 'volume_up';
+    setPlayerState((curr) => {
+      return {
+        ...curr,
+        volume: e.target.value,
+        wasVolume: e.target.value
+      }
+    })
+  }, [playerState])
+
+  return {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoSpeed,
+    toggleMute,
+    loadedData,
+    input,
+    rewind,
+    forward,
+    ended,
+    volumeChange
+  };
 };
 
 export default useVideoPlayer;
