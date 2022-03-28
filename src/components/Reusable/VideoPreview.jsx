526c36f243c39faefcf9d576696c00c24720f9d6
Melrov <rdunham918@gmail.com>
2022-03-27 19:45:12 -0500

    finished video preview

--- src/components/Reusable/VideoPreview.jsx
+++ src/components/Reusable/VideoPreview.jsx
@@ -1,12 +1,13 @@
-import { ViewSidebar } from "@mui/icons-material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./VideoPreview.css";
 
-function VideoPreview({ thumbnail, tooltip, title, username, views, uploadDate, version = "row" }) {
+function VideoPreview({ uuid, tooltip, title, username, views, uploadDate, version = "row" }) {
+  const navigate = useNavigate()
   return (
-    <div className={`container-${version === "column" ? "col" : "row"}`}>
+    <div className={`container-${version === "home" ? "home" : "row"}`} onClick={() => {navigate(`/video/${uuid}`)}} title={title}>
       <div className="thumbnail-con">
-        <img className="img" src={thumbnail} />
+        <img className="img" src={`/api/video/thumbnail/${uuid}`} />
         <div className="tooltip">{tooltip}</div>
       </div>
       <div className="info-con">
