07a0230b5e12c4406547a38a622e6d8740fa02bc
Melrov <rdunham918@gmail.com>
2022-03-27 19:46:30 -0500

    new routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import VideoPage from "./components/VideoPage";
+import UploadVideoPage from "./components/UploadVideoPage";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import { useContext, useEffect } from "react";
+import useFetch from "./hooks/useFetch";
+import { UserContext } from "./context/UserContext";
 
 function App() {
+  const { verify } = useFetch()
+  const { setLoggedInUser } = useContext(UserContext)
+  useEffect(() => {
+    async function init() {
+      const res = await verify();
+      if (res.success) {
+        setLoggedInUser(res.data.username);
+      }
+    }
+      init();
+  }, [setLoggedInUser, verify])
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<ProtectedRoutes isPrivate={false}><LoginPage /></ProtectedRoutes>} />
+        <Route path="/signup" element={<ProtectedRoutes isPrivate={false}><SignupPage /></ProtectedRoutes>} />
         <Route path="/video/:contentId" element={<VideoPage />} />
+        <Route
+          path="/upload"
+          element={
+            <ProtectedRoutes isPrivate={true}>
+              <UploadVideoPage />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
