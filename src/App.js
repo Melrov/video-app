6be2d909a136d5d449cc8f08864a277cdde38b3d
Melrov <rdunham918@gmail.com>
2022-05-07 20:12:58 -0500

    new routes and redux implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { connect } from "react-redux";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
@@ -7,44 +8,70 @@ import SignupPage from "./components/SignupPage";
 import VideoPage from "./components/VideoPage";
 import UploadVideoPage from "./components/UploadVideoPage";
 import ProtectedRoutes from "./components/ProtectedRoutes";
-import { useContext, useEffect } from "react";
-import useFetch from "./hooks/useFetch";
-import { UserContext } from "./context/UserContext";
+import { useEffect, useState } from "react";
+import useFetch from "./shared/hooks/useFetch";
 import SeriesPage from "./components/SeriesPage.jsx";
+import { setUser } from "./redux/actions/user.actions";
 
-function App() {
-  const { verify } = useFetch()
-  const { setLoggedInUser } = useContext(UserContext)
+function App({ setUser }) {
+  const [loading, setLoading] = useState(true);
+  const { verify } = useFetch();
   useEffect(() => {
     async function init() {
       const res = await verify();
       if (res.success) {
-        setLoggedInUser(res.data.username);
+        setUser(res.data.username);
       }
+      setLoading(false);
     }
-      init();
-  }, [setLoggedInUser, verify])
+    init();
+  }, [setUser, verify]);
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/login" element={<ProtectedRoutes isPrivate={false}><LoginPage /></ProtectedRoutes>} />
-        <Route path="/signup" element={<ProtectedRoutes isPrivate={false}><SignupPage /></ProtectedRoutes>} />
-        <Route path="/video/:contentId" element={<VideoPage />} />
-        <Route path="/video/:contentId/:season/:episodeNum" element={<SeriesPage />} />
-        <Route
-          path="/upload"
-          element={
-            <ProtectedRoutes isPrivate={true}>
-              <UploadVideoPage />
-            </ProtectedRoutes>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <>
+      {!loading && (
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route
+              path="/login"
+              element={
+                <ProtectedRoutes isPrivate={false}>
+                  <LoginPage />
+                </ProtectedRoutes>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <ProtectedRoutes isPrivate={false}>
+                  <SignupPage />
+                </ProtectedRoutes>
+              }
+            />
+            <Route path="/video/:contentId" element={<VideoPage />} />
+            <Route path="/series/:contentId" element={<SeriesPage />} />
+            <Route path="/series/:contentId/:season/:episodeNum" element={<SeriesPage />} />
+            <Route
+              path="/upload"
+              element={
+                <ProtectedRoutes isPrivate={true}>
+                  <UploadVideoPage />
+                </ProtectedRoutes>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {
+  setUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
